a1ce3ca3d314fa9e4f3a198192df3b6cf478e2a8
sam mcdermott <dsammcd@gmail.com>
2024-12-18 13:28:12 -0700

    Adding the final code version

--- code/main.c
+++ code/main.c
@@ -12,6 +12,7 @@ Purpose : Generic application start
 
 #include "stm32l476xx.h"
 #include <stdio.h>
+#include <math.h>
 
 /*
   ADC_ISR
@@ -25,6 +26,17 @@ int LSE_init(void);
 int MSIPLL(void);
 int PLL_init(void);
 int set_80MHz(void);
+void gen_coefs(float fc);
+int process_sample(int val);
+
+const float PI = 3.141592653589793;
+const int COEFS_M = 30;
+float coefs[31] = {0};
+
+const int BUFFER_SIZE = 32;
+const int BUFFER_LEN = 31;
+int bindex = 0;
+float buffer[32] = {0};
 
 
 /*********************************************************************
@@ -38,30 +50,24 @@ int main(void) {
   int i;
   int x = 0;
 
-  i = 100000;
-  while (--i) {
-  
-  }
-
   //printf("starting");
 
-  x = LSE_init() || MSIPLL();
-  PLL_init();
+  x = set_80MHz();
  
   RCC->AHB2ENR |= 1 | 1 << 13;
-  RCC->APB1ENR1 |= 1 << 29;
+  RCC->APB1ENR1 |= RCC_APB1ENR1_DAC1EN | RCC_APB1ENR1_TIM2EN;
   i=100;
   while (--i) {}
 
   // Set the system clock as the ADC clock
   RCC->CCIPR |= 0b11 << RCC_CCIPR_ADCSEL_Pos;
 
-  GPIOA->MODER |= 0b11;
-  GPIOA->MODER |= 0b11 << GPIO_MODER_MODE4_Pos;
+  // Set the led on the nucleo board
+  //GPIOA->MODER &= ~(0b10 << GPIO_MODER_MODE5_Pos);
+  //GPIOA->ODR |= GPIO_ODR_OD5;
 
-  GPIOA->MODER |= (0b11 << 8); // Set GPIO4 to analog
-  GPIOA->MODER &= ~(0b10 << GPIO_MODER_MODE5_Pos);
-  GPIOA->ODR |= GPIO_ODR_OD5;
+  // set gpio pins for adc and dac
+  GPIOA->MODER |= GPIO_MODER_MODE4; // Set GPIO4 to analog
   GPIOA->MODER |= 0b11; // Set GPIOA0 to analog mode
   GPIOA->ASCR |= 1; // Connect GPIOA0 to the ADC
 
@@ -70,8 +76,6 @@ int main(void) {
   GPIOA->MODER &= ~(GPIO_MODER_MODE8);
   GPIOA->MODER |= 0b10 << GPIO_MODER_MODE8_Pos;
   RCC->CFGR |= 1 << RCC_CFGR_MCOSEL_Pos;
-
-  //ADC123_COMMON->CCR |= ADC_CCR_CKMODE_Pos;
 
   ADC_init(ADC2);
 
@@ -84,24 +88,27 @@ int main(void) {
   // start ADC
   //ADC2->CR |= 4;
 
-  ADC2->CFGR2 |= 2 << ADC_CFGR2_OVSS_Pos;
-  ADC2->CFGR2 |= 0b101 << ADC_CFGR2_OVSR_Pos;
-  ADC2->CFGR2 |= ADC_CFGR2_ROVSE;
+  // ADC oversample
+  ADC2->CFGR2 |= 2 << ADC_CFGR2_OVSS_Pos; // oversample shift - 2 bits
+  ADC2->CFGR2 |= 0b101 << ADC_CFGR2_OVSR_Pos; // overample ratio - 64x
+  ADC2->CFGR2 |= ADC_CFGR2_ROVSE; // oversample enable
 
   //start DAC
   DAC->CR |= 1;
 
+  // timer for sampling
+  TIM1->ARR = 2667; // 80MHz / 2667 = approx. 30kHz, 15kHz Nyquist
+  TIM1->CR1 |= TIM_CR1_CEN;
+
   while (1) {
-    if (ADC2->ISR & ADC_ISR_EOS) {
-      ADC2->ISR |= ADC_ISR_EOS;
+    if ((TIM1->SR) & TIM_SR_UIF) {
+      TIM1->SR &= ~TIM_SR_UIF;
       x = ADC2->DR;
-      DAC->DHR12L1 = x;
-    }
-    if ((ADC2->CR & (ADC_CR_ADSTART | ADC_CR_ADDIS | ADC_CR_ADEN)) == ADC_CR_ADEN) {
+      while(ADC2->CR & ADC_CR_ADSTART);
       ADC2->CR |= ADC_CR_ADSTART;
+      DAC->DHR12L1 = process_sample(x) & ~0b1111;
     }
   }
-
 }
 
 
@@ -132,6 +139,7 @@ int ADC_init(ADC_TypeDef *ADC) {
   return 0;
 }
 
+// initialize the lse crystal
 int LSE_init(void) {
   int i;
   if (!(RCC->APB1ENR1 & RCC_APB1ENR1_PWREN)) {
@@ -152,6 +160,7 @@ int LSE_init(void) {
   return 0;
 }
 
+// trim the msi to the lse
 int MSIPLL(void) {
   if (!(RCC->BDCR & (RCC_BDCR_LSEON | RCC_BDCR_LSERDY)))
     return -1;
@@ -166,7 +175,7 @@ int PLL_init(void) {
   RCC->CR &= ~RCC_CR_PLLON;
   while (RCC->CR & RCC_CR_PLLRDY) {}
   RCC->PLLCFGR = 0; // Hopefully this is fine, since PLLN gets set to a valid state before the PLL is reactivated
-  RCC->PLLCFGR |= 24 << RCC_PLLCFGR_PLLN_Pos; // 40 * 4MHz = 160MHz. 160MHz / 2 = 80MHz
+  RCC->PLLCFGR |= 40 << RCC_PLLCFGR_PLLN_Pos; // 40 * 4MHz = 160MHz. 160MHz / 2 = 80MHz
   RCC->PLLCFGR |= RCC_PLLCFGR_PLLSRC_MSI_Msk;
   RCC->CR |= RCC_CR_PLLON;
   RCC->PLLCFGR |= RCC_PLLCFGR_PLLREN;
@@ -177,9 +186,48 @@ int PLL_init(void) {
 }
 
 int set_80MHz(void) {
+  // reference manual 3.3.3
   PWR->CR1 = (PWR->CR1 & ~PWR_CR1_VOS_Msk) | (1 << PWR_CR1_VOS_Pos);
-  while (!(PWR->SR2 & PWR_SR2_VOSF));
-  FLASH->ACR |= 
+  while (PWR->SR2 & PWR_SR2_VOSF);
+  FLASH->ACR |= 4;
+  if ((FLASH->ACR & FLASH_ACR_LATENCY) != 4)
+    return -1;
+  return LSE_init() || MSIPLL() || PLL_init();
+}
 
+// fc is fraction of sampling frequency from 0 to 0.5
+// generates coefficients for a windowed-sinc low-pass filter with the given cutoff frequency
+// The Scientist & Engineer's Guide to Digital Signal Processing, chapter 16
+void gen_coefs(float fc) {
+  int i;
+  int rel;
+  int midpoint = COEFS_M / 2;
+  float c1a = 2 * PI * COEFS_M;
+  float c2a = 2 * c1a;
+  float ang_freq = 2 * PI * fc;
+  if (fc < 0 || fc > 0.5)
+    return;
+  for (i = 0; i <= COEFS_M; ++i) {
+    if (i == midpoint)
+      coefs[COEFS_M - i] = 2 * PI * fc;
+    else {
+      rel = i - midpoint;
+      coefs[COEFS_M - i] = sinf(midpoint * rel) * (0.42 - 0.5 * cosf(c1a * i) + 0.08 * cosf(c2a * i)) / rel;
+    }
+  }
+}
+
+// adds the sample to the circular buffer and runs the filter to get the next output value
+int process_sample(int val) {
+  int i;
+  float acc = 0;
+  float sample = (float)val / 65536;
+  buffer[(bindex + BUFFER_LEN) & 32] = sample;
+  ++bindex;
+  for (i = 0; i < BUFFER_LEN; ++i) {
+    acc += coefs[i] * buffer[(bindex + i) % 32];
+  }
+  return (int)(65536 * acc);
+}
 
 /*************************** End of file ****************************/
